15c2ce72c1faf85f670b8227e09a0084790693fe
hoy <ngkon172@gmail.com>
2015-10-09 22:45:08 +0900

    2015/05/19

--- src/main/java/com/mamababy/Application.java
+++ src/main/java/com/mamababy/Application.java
@@ -3,6 +3,7 @@ package com.mamababy;
 import com.mamababy.admin.Interceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
@@ -27,6 +28,7 @@ import java.nio.charset.Charset;
 @ComponentScan
 @EnableAutoConfiguration
 @Controller
+@SpringBootApplication
 public class Application extends SpringBootServletInitializer {
     @Override
         protected SpringApplicationBuilder configure(SpringApplicationBuilder application){
